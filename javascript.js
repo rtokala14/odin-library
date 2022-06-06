62b47d05f5148dd89c94035cd37453eb9b7de4af
Rohit Tokala <rtokala@purdue.edu>
2022-06-07 03:04:50 +0400

    Add form functionality

--- javascript.js
+++ javascript.js
@@ -1,19 +1,47 @@
 let myLibrary = [];
 
 const container = document.querySelector('.card-container');
+const addBtn = document.querySelector('#bookBtn');
+const bookModal = document.querySelector('#bookModal');
+const bookForm = document.querySelector('#addBookForm');
+const overlay = document.querySelector('#overlay');
 
-function Book(name, author) {
-    this.name = name;
-    this.author = author;
-    this.read = false;
+addBtn.onclick = () => {
+    bookForm.reset();
+    bookModal.classList.add('active');
+    overlay.classList.add('active');
 }
 
-function addBookToLibrary(name, author) {
-    let book = new Book(name, author);
+function closeModal() {
+    bookModal.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
+overlay.onclick = () => closeModal();
+
+bookForm.onsubmit = (e) => {
+    e.preventDefault();
+    const v1 = document.getElementById('input-title').value;
+    const v2 = document.getElementById('input-author').value;
+    const v3 = document.getElementById('input-read').value;
+    const newBook = new Book(v1, v2, v3);
+    addBookToLibrary(newBook);
+    closeModal();
+}
+
+function Book(name, author, read) {
+    this.name = name;
+    this.author = author;
+    this.read = (read == 'on') ? true : false;
+}
+
+function addBookToLibrary(book) {
     myLibrary.push(book);
+    displayBooks();
 }
 
 function displayBooks() {
+    container.textContent='';
     myLibrary.forEach( (book) => {
         let card = document.createElement('div');
         card.classList.add('card');
@@ -40,7 +68,7 @@ function displayBooks() {
     })
 }
 
-window.onload = () => {
+/*window.onload = () => {
     addBookToLibrary('Dune', 'Frank Herbert');
     displayBooks();
-}
+}*/
